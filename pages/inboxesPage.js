c001a2c8ac060c03fb717a17a471cadf76a131f8
suamorales <morales.sua@gmail.com>
2016-11-15 00:00:10 -0500

    Finish testInboxes test. Add checking for successful messaging.

--- pages/inboxesPage.js
+++ pages/inboxesPage.js
@@ -15,19 +15,21 @@ export default {
   sections: {
     conversationListWrapper: {
       selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD',
-      filterContainter: {
-        selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div',
-        filterCount: {
-          selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div > div.view-count'
-        },
-        filterTitle: {
-          selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div > span'
-        },
-        filterDropdownArrow: {
-          selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div > i'
-        },
-        filterMenu: {
-          selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > div'
+      elements: {
+        filterContainter: {
+          selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div',
+          filterCount: {
+            selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div > div.view-count'
+          },
+          filterTitle: {
+            selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div > span'
+          },
+          filterDropdownArrow: {
+            selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > span > div > i'
+          },
+          filterMenu: {
+            selector: '#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.ConversationListWrapper---1FLXD > div.ViewSelectorDropdown---3YSqV > div > div'
+          }
         }
       },
       conversationListItem: {
@@ -40,6 +42,9 @@ export default {
         },
         listItemTag: {
           selector: '#conversation-list > div.ConversationListItem.selected.open > div.TagList > div'
+        },
+        conversationSummary: {
+          selector: '#conversation-list > div.ConversationListItem.selected.open > div.conversation-summaries > div.summary'
         }
       }
     },
--- tests/testInboxes.js
+++ tests/testInboxes.js
@@ -14,13 +14,21 @@ before(client) {
     client.end();
   },
 
-  'User Navigates to Inboxe': (client) => {
+  'User Navigates to Inboxes. All sections should be visible': (client) => {
     const inboxesPage = client.page.inboxesPage();
     inboxesPage.navigate();
 
     inboxesPage.expect.section('@conversationListWrapper').to.be.visible;
-    // inboxesPage.expect.section('@messageSectionWrapper').to.be.visible;
-    // inboxesPage.expect.section('@textAreaDropZone').to.be.visible;
-    //inboxesPage.expect.section('@textAreaDropZone').to.be.visible;
+    inboxesPage.expect.section('@messageSectionWrapper').to.be.visible;
+    inboxesPage.expect.section('@textAreaDropZone').to.be.visible;
+
+    client.setValue('#textAreaComponent', 'Drift_test');
+    
+    // Add element to inboxesPage.js
+    //send button
+    client.click('#app > div > div.AppContainer---haZ5V > div.App---3HNzd > div:nth-child(1) > div > div.Conversation > div.text-area-drop-zone > div > div.text-area > div.Wrapper---3YQMA.composer-actions.flex-space-between > div:nth-child(2) > div:nth-child(4) > div > div.dsg-Body.color-blue')
+    
+    // conversationSummary
+    inboxesPage.assert.containsText('#conversation-list > div.ConversationListItem.selected.open > div.conversation-summaries > div','Drift_test');
   }
 };
